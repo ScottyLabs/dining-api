4e2385c035be661cd9cae33911ec7b16385246b7
topshenyi-web <erxu@ctemc.org>
2024-09-08 21:39:29 -0400

    chore: cleanup

--- src/utils/coordinate.ts
+++
@@ -1,8 +0,0 @@
-export function convertMapsLinkToCoordinates(link: string): [number, number] {
-  const atIndex = link.indexOf("@");
-  const locationUrl = link.slice(atIndex + 1, link.length);
-  const commaIndex = locationUrl.indexOf(",");
-  const latitude = locationUrl.slice(0, commaIndex);
-  const longitude = locationUrl.slice(commaIndex + 1, locationUrl.length);
-  return [parseFloat(latitude), parseFloat(longitude)];
-}
