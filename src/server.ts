6c129376d4271ff5ea3de2b7203bce59bcc52ede
ajxu2 <63217938+ajxu2@users.noreply.github.com>
2024-12-04 23:59:57 -0600

    Correctly refresh at 11:45PM every day (#163)

--- src/server.ts
+++ src/server.ts
@@ -71,9 +71,17 @@ app.get("/locations/time/:day/:hour/:min", ({ params: { day, hour, min } }) => {
 // (for example, we don't want to start scraping at 11:59:59).
 const interval = 1000 * 60 * 10;
 setInterval(() => {
-  const today = new Date();
-  const hr = today.getHours();
-  const min = today.getMinutes();
+  // ensure we get the correct time zone
+  // TODO: can anyone find a better solution to this...
+  const timeStr: String = new Date().toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: false,
+    timeZone: "America/New_York",
+  }); // this returns a string [hours]:[minutes]
+  const hm = timeStr.split(":").map((x) => parseInt(x));
+  const hr = hm[0];
+  const min = hm[1];
   if (hr == 23 && 45 <= min && min < 55) {
     reload().catch(console.error);
   }
