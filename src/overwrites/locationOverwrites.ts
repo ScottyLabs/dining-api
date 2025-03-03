6510556ebadbb1d6eb8d2151cc0cb399eb3d09bd
Jaisal Patel <70134418+GhostOf0days@users.noreply.github.com>
2025-03-03 04:33:26 -0500

    fix grammar

--- src/overwrites/locationOverwrites.ts
+++ src/overwrites/locationOverwrites.ts
@@ -3,7 +3,7 @@ import { ICoordinate,ILocationCoordinateOverwrites } from "types";
 const locationCoordinateOverwrites = require("./locationOverwrites.json") as ILocationCoordinateOverwrites;
 
 /**
- * Overwrite dining locations coordinates because they are wrong on the dining website.
+ * Overwrite dining locations' coordinates because they are wrong on the dining website.
  */
 const locationCoordinates = new Map<number, ICoordinate>(
     Object.entries(locationCoordinateOverwrites).map(([conceptIdString, coordinates]) => {
