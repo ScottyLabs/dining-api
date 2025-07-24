08ceffad891e7e71d39e86004c5946e22d7c6d5c
topshenyi-web <erxu@ctemc.org>
2025-07-24 16:31:15 -0400

    test: add LocationMerger tests because I don't trust myself

--- src/server.ts
+++ src/server.ts
@@ -7,49 +7,9 @@ import { notifySlack } from "utils/slack";
 import { node } from "@elysiajs/node";
 import { getDiffsBetweenLocationData } from "utils/diff";
 import { getEmails } from "./db";
+import LocationMerger from "utils/locationMerger";
 
 let cachedLocations: ILocation[] = [];
-
-class LocationMerger {
-  majorityDict: Partial<
-    Record<
-      number,
-      Partial<Record<string, { cnt: number; originalData: ILocation }>>
-    >
-  > = {}; // Partial type because the values don't exist initially
-  addLocation(location: ILocation) {
-    const majorityDictLocationData =
-      this.majorityDict[location.conceptId] ?? {};
-    const hashedVersion = JSON.stringify(location);
-
-    majorityDictLocationData[hashedVersion] = {
-      cnt: (majorityDictLocationData[hashedVersion]?.cnt ?? 0) + 1,
-      originalData: location, // theoretically this should be the same as all previous versions
-    };
-    this.majorityDict[location.conceptId] = majorityDictLocationData;
-  }
-  getMostFrequentLocations() {
-    return Object.entries(this.majorityDict).map(([conceptId, freqData]) => {
-      if (freqData === undefined) {
-        throw new Error(`Expected frequency data for concept id ${conceptId}`);
-      }
-
-      const bestMatch = Object.values(freqData).reduce((bestMatch, curVal) => {
-        if (curVal === undefined) throw new Error();
-        return bestMatch === undefined || curVal.cnt > bestMatch.cnt
-          ? curVal
-          : bestMatch;
-      }, undefined);
-      if (bestMatch === undefined) throw new Error();
-      console.log(
-        `${bestMatch.originalData.name} frequencies: ${Object.values(
-          freqData
-        ).map((val) => val?.cnt)}`
-      );
-      return bestMatch.originalData;
-    });
-  }
-}
 
 async function reload(): Promise<void> {
   const now = new Date();
