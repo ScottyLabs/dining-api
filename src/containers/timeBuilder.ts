eddba6f5a367e38d1719ddf137616b64dfa23efb
topshenyi-web <erxu@ctemc.org>
2025-08-08 16:31:42 -0400

    fix: coalesce intervals if gap is 1 min, and do wrap-around coalescing

--- src/containers/timeBuilder.ts
+++ src/containers/timeBuilder.ts
@@ -28,8 +28,7 @@ export function getTimeRangesFromString(rowHTML: Element) {
 }
 
 function getTimeAttributesFromRow(rowHTML: Element) {
-  const { day, date, timeSlots } = tokenizeTimeRow(rowHTML);
-  return getTimeInfoWithRawAttributes([day, date, ...timeSlots]);
+  return getTimeInfoWithRawAttributes(tokenizeTimeRow(rowHTML));
 }
 
 function tokenizeTimeRow(rowHTML: Element) {
@@ -37,12 +36,13 @@ function tokenizeTimeRow(rowHTML: Element) {
   let day = $("strong").text();
   const dataStr = $.text().replace(/\s\s+/g, " ").replace(day, "").trim();
   let [date, time] = dataStr.split(/,(.+)/);
+  if (date === undefined || time === undefined) return [];
 
   day = (day.charAt(0).toUpperCase() + day.slice(1).toLowerCase()).trim();
   date = (date.charAt(0).toUpperCase() + date.slice(1).toLowerCase()).trim();
   time = time.toUpperCase().trim();
   const timeSlots = time.split(/[,;]/).map((slot) => slot.trim());
-  return { day, date, timeSlots };
+  return [day, date, ...timeSlots];
 }
 
 function getTimeInfoWithRawAttributes(tokens: string[]) {
@@ -108,12 +108,16 @@ function resolveAttributeConflicts(
 
 function getTimeRangesFromTimeRow(time: ITimeRowAttributes) {
   if (time.day === undefined) {
-    throw new Error("Cannot convert when day is not set");
+    notifySlack(
+      `<!channel> Cannot convert time attribute: ${JSON.stringify(
+        time
+      )} since day is not set`
+    );
+    return [];
   }
   const allRanges: ITimeRange[] = [];
   for (const range of time.times ?? []) {
-    rollBack12AmEndTime(range);
-
+    rollBack12AmEndTime(range); // not sure why this was added, but it doesn't hurt I guess (I suppose the only case this actively helps is if the time string is 12:00 AM - 12:00 AM)
     const shouldSpillToNextDay =
       range.start.hour * 60 + range.start.minute >
       range.end.hour * 60 + range.end.minute;
--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -1,4 +1,4 @@
-import { DayOfTheWeek, ITimeMoment, ITimeRange } from "types";
+import { DayOfTheWeek, ITimeSlot, ITimeRange } from "types";
 
 export function getNextDay(day: DayOfTheWeek): DayOfTheWeek {
   const weekdays: DayOfTheWeek[] = [
@@ -13,7 +13,7 @@ export function getNextDay(day: DayOfTheWeek): DayOfTheWeek {
   return weekdays[(weekdays.indexOf(day) + 1) % 7];
 }
 
-export function getMinutesSinceStartOfSunday(timeMoment: ITimeMoment) {
+export function getMinutesSinceStartOfSunday(timeMoment: ITimeSlot) {
   return timeMoment.day * (24 * 60) + timeMoment.hour * 60 + timeMoment.minute;
 }
 /**
@@ -22,7 +22,7 @@ export function getMinutesSinceStartOfSunday(timeMoment: ITimeMoment) {
  * @param moment2
  * @returns Delta in minutes of moment1 - moment2
  */
-export function compareTimeMoments(moment1: ITimeMoment, moment2: ITimeMoment) {
+export function compareTimeSlots(moment1: ITimeSlot, moment2: ITimeSlot) {
   return (
     getMinutesSinceStartOfSunday(moment1) -
     getMinutesSinceStartOfSunday(moment2)
@@ -31,7 +31,7 @@ export function compareTimeMoments(moment1: ITimeMoment, moment2: ITimeMoment) {
 
 export function sortAndMergeTimeRanges(timeRanges: ITimeRange[]) {
   timeRanges.sort((range1, range2) =>
-    compareTimeMoments(range1.start, range2.start)
+    compareTimeSlots(range1.start, range2.start)
   );
   const mergedRanges: ITimeRange[] = [];
 
@@ -39,15 +39,53 @@ export function sortAndMergeTimeRanges(timeRanges: ITimeRange[]) {
     const lastTimeRange = mergedRanges.length
       ? mergedRanges[mergedRanges.length - 1]
       : undefined;
+    if (compareTimeSlots(timeRange.start, timeRange.end) > 0) {
+      // this is a wrap-around time (we, however, don't want to deal with these when merging time intervals (the logic may be a bit messy), so let's just un-wrap the time)
+      timeRange.end.day += 7; // no longer a valid DayOfTheWeek, but that's fine
+    }
     if (
       lastTimeRange &&
-      compareTimeMoments(lastTimeRange.end, timeRange.start) >= 0
+      compareTimeSlots(lastTimeRange.end, timeRange.start) >= -1 // we overlap 1-minute disjoint intervals as well (ex. 2:00 PM - 2:59 PM will get merged with 3:00PM - 4:00PM as 2:00PM - 4:00PM)
     ) {
-      if (compareTimeMoments(timeRange.end, lastTimeRange.end) > 0) {
+      if (compareTimeSlots(timeRange.end, lastTimeRange.end) > 0) {
         lastTimeRange.end = timeRange.end; // join current range with last range
       }
     } else {
       mergedRanges.push(timeRange);
+    }
+  }
+  for (const mergedRange of mergedRanges) {
+    if (compareTimeSlots(mergedRange.start, mergedRange.end) > 0)
+      throw new Error("unexpected wrap-around range found!");
+  }
+  // last timeRange should be the only one that wraps around (if it does), since no starting time slot has day >= 7
+  if (mergedRanges.length > 0) {
+    const lastTimeSlot = mergedRanges[mergedRanges.length - 1];
+    if (lastTimeSlot.end.day >= 7) {
+      // this is a wrap-around - need to coalesce with the earlier merged ranges
+      lastTimeSlot.end.day %= 7;
+      if (compareTimeSlots(lastTimeSlot.end, lastTimeSlot.start) >= -1) {
+        // the last merged time slot covers the entire week
+        return [
+          {
+            start: { day: DayOfTheWeek.SUNDAY, hour: 0, minute: 0 },
+            end: { day: DayOfTheWeek.SATURDAY, hour: 23, minute: 59 },
+          },
+        ];
+      } else {
+        // merge it with the earlier time ranges (note that the last time interval cannot merge with itself due to the above if condition)
+        while (mergedRanges.length > 1) {
+          if (compareTimeSlots(lastTimeSlot.end, mergedRanges[0].start) >= -1) {
+            lastTimeSlot.end =
+              compareTimeSlots(lastTimeSlot.end, mergedRanges[0].end) > 0
+                ? lastTimeSlot.end
+                : mergedRanges[0].end;
+            mergedRanges.shift();
+          } else {
+            break;
+          }
+        }
+      }
     }
   }
   return mergedRanges;
